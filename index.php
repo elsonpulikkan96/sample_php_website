207ca96cce6ec6fd9bf59693ffa8f4446bc686fd
elsonpulikkan96 <118882519+elsonpulikkan96@users.noreply.github.com>
2023-12-10 11:47:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,80 +1,109 @@
 <?php
-  
 // Database Connection
-
 $user = 'admin';
-$password = 'admin1234'; 
-$database = 'college'; 
-$servername='localhost:3306';
+$password = 'admin1234';
+$database = 'college';
+$servername = 'localhost:3306';
 
-$mysqli = new mysqli($servername, $user, 
-                $password, $database);
-  
+$mysqli = new mysqli($servername, $user, $password, $database);
+
 // Checking for connections
 if ($mysqli->connect_error) {
-    die('Connect Error (' . 
-    $mysqli->connect_errno . ') '. 
-    $mysqli->connect_error);
+    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
-  
+
 // SQL query to select data from database
 $sql = "SELECT * FROM students ";
 $result = $mysqli->query($sql);
-$mysqli->close(); 
+$mysqli->close();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  
+
 <head>
     <meta charset="UTF-8">
-
-    <title>This Version 1</title>
-
-    <!-- CSS FOR STYLING THE PAGE -->
-
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>College Students</title>
     <style>
-        table {
-            margin: 0 auto;
-            font-size: large;
-            border: 1px solid black;
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
         }
-  
-        h1 {
+
+        header {
+            background-color: #3498db;
+            color: #fff;
             text-align: center;
-            color: #006600;
-            font-size: xx-large;
-            font-family: 'Gill Sans', 'Gill Sans MT', 
-            ' Calibri', 'Trebuchet MS', 'sans-serif';
+            padding: 1em 0;
+            border-bottom: 2px solid #2980b9;
         }
-  
-        td {
-            background-color: #E4F5D4;
-            border: 1px solid black;
+
+        section {
+            max-width: 800px;
+            margin: 20px;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
-  
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
         th,
         td {
-            font-weight: bold;
-            border: 1px solid black;
-            padding: 10px;
-            text-align: center;
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: left;
         }
-  
-        td {
-            font-weight: lighter;
+
+        th {
+            background-color: #3498db;
+            color: #fff;
+            font-weight: bold;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        h1 {
+            text-align: center;
+            color: #3498db;
+            margin-bottom: 20px;
+        }
+
+        /* Responsive Design */
+        @media only screen and (max-width: 600px) {
+            body {
+                padding: 10px;
+            }
+
+            section {
+                margin: 10px;
+            }
+
+            table {
+                font-size: 14px;
+            }
         }
     </style>
 </head>
-  
+
 <body>
+    <header>
+        <h1>College Students Information</h1>
+    </header>
     <section>
-      
-             <?php
-$output = shell_exec('echo $HOSTNAME');
-echo "<h1><center><pre>$output</pre></center></h1>";
-echo "<h1><center><pre> ajishantony.tech  version - 2 </pre></center></h1>";
-?>
         <!-- TABLE CONSTRUCTION-->
         <table>
             <tr>
@@ -83,22 +112,16 @@ echo "<h1><center><pre> ajishantony.tech  version - 2 </pre></center></h1>";
                 <th>Email</th>
             </tr>
             <!-- PHP CODE TO FETCH DATA FROM ROWS-->
-            <?php   // LOOP TILL END OF DATA 
-                while($rows=$result->fetch_assoc())
-                {
-             ?>
-            <tr>
-                <!--FETCHING DATA FROM EACH 
-                    ROW OF EVERY COLUMN-->
-                <td><?php echo $rows['name'];?></td>
-                <td><?php echo $rows['age'];?></td>
-                <td><?php echo $rows['email'];?></td>
-            </tr>
-            <?php
-                }
-             ?>
+            <?php while ($rows = $result->fetch_assoc()) { ?>
+                <tr>
+                    <!--FETCHING DATA FROM EACH ROW OF EVERY COLUMN-->
+                    <td><?php echo $rows['name']; ?></td>
+                    <td><?php echo $rows['age']; ?></td>
+                    <td><?php echo $rows['email']; ?></td>
+                </tr>
+            <?php } ?>
         </table>
     </section>
 </body>
-  
+
 </html>
